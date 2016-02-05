1e506ef76856eca7208ab10474d3b4101fc03248
klarkc <walker@praiseweb.com.br>
2016-02-05 12:26:02 -0300

    feat(task): Implements init task

--- tasks/init.js
+++ tasks/init.js
@@ -1,1 +1,28 @@
 "use strict";
+
+var git = require("gulp-git");
+var gutil = require("gulp-utils");
+var runSequence = require("run-sequence");
+var $ = require("../util.js");
+
+var developBranch = function (next) {
+  $.askContinue("We gonna create the develop branch, if this branch already exists the branch will be reset to HEAD. Continue?", function () {
+    git.checkout("develop", {
+      args: "-B"
+    }, next);
+  }, true);
+};
+
+var afterInstructions = function(next){
+  gutil.info("We created a release branch, please review the release branch and if it's ok, execute: $ gulp finish-release");
+  next();
+};
+
+module.exports = function (done) {
+  runSequence(
+    developBranch,
+    "start-release",
+    afterInstructions,
+    done
+  );
+};
