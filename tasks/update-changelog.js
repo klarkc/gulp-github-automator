6624311afde74f2b27a2efdde430703dfd4f98cc
klarkc <walker@praiseweb.com.br>
2016-02-05 12:25:42 -0300

    feat(task): Add file verification to update-changelog

--- tasks/update-changelog.js
+++ tasks/update-changelog.js
@@ -6,13 +6,30 @@ var path = require("path");
 var $ = require("../util.js");
 
 module.exports = function () {
-    var src = gulp.src(path.resolve($.conf.appDir, "./CHANGELOG.md"), {
-        buffer: false
+  var file = path.resolve($.conf.appDir, "./CHANGELOG.md");
+  var src;
+
+  if (path.accessSync(file)) {
+    // If file exists
+    src = gulp.src(file, {
+      buffer: false
     });
     return src
-        .pipe(changelog({
-            preset: $.conf.preset
-        }))
-        .pipe(gulp.dest($.conf.appDir));
+      .pipe(changelog({
+        preset: $.conf.preset
+      }))
+      .pipe(gulp.dest($.conf.appDir));
+  } else {
+    // If file do not exists
+    src = gulp.src(file, {
+      buffer: false,
+      read: false
+    });
+    return src
+      .pipe(changelog({
+        preset: $.conf.preset,
+        releaseCount: 0
+      }))
+      .pipe(gulp.dest($.conf.appDir));
+  }
 };
-
