dd199c5d51f0b7c97f5199389791588f16e53f81
klarkc <walker@praiseweb.com.br>
2016-02-05 11:19:13 -0300

    feat(utility): Add merge function

--- util.js
+++ util.js
@@ -88,3 +88,29 @@ $.askPushTo = function (local, remote, done) {
     }, done);
   });
 };
+
+/*
+* Recursively merge properties of two objects
+*/
+$.merge = function (obj1, obj2) {
+
+  for (var p in obj2) {
+    try {
+      // Property in destination object set; update its value.
+      if ( obj2[p].constructor==Object ) {
+        obj1[p] = $.merge(obj1[p], obj2[p]);
+
+      } else {
+        obj1[p] = obj2[p];
+
+      }
+
+    } catch(e) {
+      // Property in destination object not set; create it and set its value.
+      obj1[p] = obj2[p];
+
+    }
+  }
+
+  return obj1;
+};
