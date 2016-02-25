8d15a5eb4dc8cc531947c24eb6c70f004bad3768
klarkc <walker@praiseweb.com.br>
2016-02-25 15:51:13 -0300

    perf(tests): Fix code indentation

--- tests/util.test.js
+++ tests/util.test.js
@@ -5,11 +5,14 @@ var assertions = require('./custom-assertions');
 var mockery = require("mockery");
 var path = require("path");
 var git = require("gulp-git");
-var gitOptions = {quiet: true};
 var fs = require("fs");
 
-describe("util.js", function(){
-  it("should have a default conf", function(){
+var gitOptions = {
+  quiet: true
+};
+
+describe("util.js", function () {
+  it("should have a default conf", function () {
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
@@ -20,31 +23,31 @@ describe("util.js", function(){
     should(util.conf.versionFiles.length).greaterThan(0);
   });
 
-  it("should createTmpBranch", function(done){
+  it("should createTmpBranch", function (done) {
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
-    util.createTmpBranch(function(name){
+    util.createTmpBranch(function (name) {
       should(name).match(/^tmp\-\d+$/);
       done();
     }, gitOptions);
   });
 
-  it("should read packageVersion", function() {
+  it("should read packageVersion", function () {
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
     should(util.packageVersion('package.json')).match(/^(\d+\.)?\d+.\d+$/);
   });
 
-  it("should commitChangesStream", function(done){
+  it("should commitChangesStream", function (done) {
     var util = require("../util.js");
 
     fs.writeFileSync('deleteme.md', 'please, deleteme');
     util.conf.appDir = 'deleteme.md';
 
     var stream = util.commitChangesStream(gitOptions);
-    stream.on('finish', function(){
+    stream.on('finish', function () {
       should(process.env.SANDBOX_DIR).containsGitLog(
         "[Prerelease] Bumped version number",
         done
@@ -52,18 +55,18 @@ describe("util.js", function(){
     });
   });
 
-  it("should not mergeInto without -b", function(){
+  it("should not mergeInto without -b", function () {
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
     should(util.mergeInto).throw(Error);
   });
 
-  it("should test branch mergeInto master", function(done){
+  it("should test branch mergeInto master", function (done) {
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
-    util.mergeInto("test", function(){
+    util.mergeInto("test", function () {
       should(process.env.SANDBOX_DIR).containsGitLog(
         "Merged",
         done
