b287c4f80696353edd0b0290fff8f1e1b3aaf827
klarkc <walker@praiseweb.com.br>
2016-02-25 15:45:38 -0300

    feat(tests): Implements mock for cli arguments and implements git bootstrap

--- tests/bootstrap.test.js
+++ tests/bootstrap.test.js
@@ -3,10 +3,16 @@ var fs = require("fs");
 var resolve = require("path").resolve;
 var rmdir = require("rimraf");
 var mockery = require("mockery");
+var git = require("gulp-git");
+var gulp = require("gulp");
 
 var sandboxDir = process.env.SANDBOX_DIR;
 
 var mochaCwd = process.cwd();
+
+var gitOptions = {
+  quiet: true
+};
 
 function writeFiles() {
   fs.writeFileSync("package.json", "{\n\"version\": \"1.2.3\"\n}");
@@ -16,30 +22,51 @@ before(function () {
   if (!sandboxDir) {
     sandboxDir = process.env.SANDBOX_DIR = resolve(__dirname, "test_sandbox");
   }
-});
-
-beforeEach(function () {
-  fs.mkdirSync(sandboxDir);
-  process.chdir(resolve(sandboxDir));
-  writeFiles();
 
   mockery.enable({
     warnOnUnregistered: false
   });
+
+  var mockArgs = {
+    argv: {
+      b: 'test'
+    }
+  };
+  mockery.registerMock('yargs', mockArgs);
+});
+
+beforeEach(function (done) {
+  fs.mkdirSync(sandboxDir);
+  process.chdir(resolve(sandboxDir));
+  writeFiles();
+
+  // Init Git
+  git.init(gitOptions, function () {
+    // Add and commit
+    var gitAddCommit = gulp.src("*")
+      .pipe(git.add(gitOptions))
+      .pipe(git.commit("first commit", gitOptions));
+
+    gitAddCommit.on('finish', function(){
+      // Test branch
+      git.branch('test', gitOptions, done);
+    });
+  });
 });
 
 afterEach(function (done) {
-  mockery.disable();
   process.chdir(mochaCwd);
   rmdir(resolve(sandboxDir), done);
 });
 
-after(function(done){
+after(function (done) {
+  mockery.disable();
+
   // If something gone wrong we force-removal of the test directory, we don't want this directory messing up future tests.
   try {
     rmdir(resolve(sandboxDir), done);
     throw new Error("Test sandbox directory has been forced deleted");
-  } catch(err) {
+  } catch (err) {
     done();
   }
 });
