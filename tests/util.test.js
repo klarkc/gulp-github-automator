21ba470d982218e9a64937075943f4bf5eadc25d
klarkc <walker@praiseweb.com.br>
2016-02-25 15:46:47 -0300

    fix(tests): Update the test to support modifications in util regarding opts variable and fix commitChangesStream test

--- tests/util.test.js
+++ tests/util.test.js
@@ -5,7 +5,8 @@ var assertions = require('./custom-assertions');
 var mockery = require("mockery");
 var path = require("path");
 var git = require("gulp-git");
-var gitOptions = {args: '--quiet', quiet: true};
+var gitOptions = {quiet: true};
+var fs = require("fs");
 
 describe("util.js", function(){
   it("should have a default conf", function(){
@@ -23,12 +24,10 @@ describe("util.js", function(){
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
-    git.init(gitOptions);
-
     util.createTmpBranch(function(name){
       should(name).match(/^tmp\-\d+$/);
       done();
-    }, gitOptions.args);
+    }, gitOptions);
   });
 
   it("should read packageVersion", function() {
@@ -40,13 +39,11 @@ describe("util.js", function(){
 
   it("should commitChangesStream", function(done){
     var util = require("../util.js");
-    util.conf.appDir = process.env.SANDBOX_DIR;
 
-    util.conf.appDir = '*';
+    fs.writeFileSync('deleteme.md', 'please, deleteme');
+    util.conf.appDir = 'deleteme.md';
 
-    git.init(gitOptions);
-
-    var stream = util.commitChangesStream(gitOptions.args);
+    var stream = util.commitChangesStream(gitOptions);
     stream.on('finish', function(){
       should(process.env.SANDBOX_DIR).containsGitLog(
         "[Prerelease] Bumped version number",
@@ -56,28 +53,22 @@ describe("util.js", function(){
   });
 
   it("should not mergeInto without -b", function(){
-    var mockArgs = {
-      argv: {}
-    };
-
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
     should(util.mergeInto).throw(Error);
   });
 
-  it("should -b test mergeInto master", function(){
-    var mockArgs = {
-      argv: {b: 'test'}
-    };
-
-    mockery.registerMock('yargs', mockArgs);
+  it("should test branch mergeInto master", function(done){
     var util = require("../util.js");
     util.conf.appDir = process.env.SANDBOX_DIR;
 
     util.mergeInto("test", function(){
-
-    }, gitOptions.args);
+      should(process.env.SANDBOX_DIR).containsGitLog(
+        "Merged",
+        done
+      );
+    }, gitOptions);
   });
 
 });
