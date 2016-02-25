7b14ca3811fed256c0f458d0e9334538fd511728
klarkc <walker@praiseweb.com.br>
2016-02-25 14:04:57 -0300

    feat(tests): Add support for mockery, truly remove tests directory, even if something brokes

--- tests/bootstrap.test.js
+++ tests/bootstrap.test.js
@@ -2,6 +2,7 @@
 var fs = require("fs");
 var resolve = require("path").resolve;
 var rmdir = require("rimraf");
+var mockery = require("mockery");
 
 var sandboxDir = process.env.SANDBOX_DIR;
 
@@ -21,17 +22,24 @@ beforeEach(function () {
   fs.mkdirSync(sandboxDir);
   process.chdir(resolve(sandboxDir));
   writeFiles();
+
+  mockery.enable({
+    warnOnUnregistered: false
+  });
 });
 
 afterEach(function (done) {
+  mockery.disable();
   process.chdir(mochaCwd);
   rmdir(resolve(sandboxDir), done);
 });
 
 after(function(done){
-  if(fs.statSync(resolve(sandboxDir)).isDirectory()) {
+  // If something gone wrong we force-removal of the test directory, we don't want this directory messing up future tests.
+  try {
     rmdir(resolve(sandboxDir), done);
-  } else {
+    throw new Error("Test sandbox directory has been forced deleted");
+  } catch(err) {
     done();
   }
 });
