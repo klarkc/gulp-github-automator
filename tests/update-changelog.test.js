d0015ffec2af20aebfe383e9f5bcce00f26bb05f
test <walker@praiseweb.com.br>
2016-03-06 18:30:44 -0300

    feat(tests): Add update-changelog test

--- tests/update-changelog.test.js
+++ tests/update-changelog.test.js
@@ -1,1 +1,28 @@
 "use strict";
+var should = require("should");
+var updChangelog = require("../tasks/update-changelog.js");
+var fs = require("fs");
+
+describe("Task update-changelog", function(){
+  it("should create changelog file", function(done){
+    var util = require("../util.js");
+    util.conf.appDir = global.sandboxDir;
+
+    var gulpStream = updChangelog();
+    gulpStream.on("end", function(){
+      var file = fs.readFileSync("./CHANGELOG.md");
+      console.log(file);
+      done();
+    });
+  });
+  it("should update existing changelog file", function(done){
+    var util = require("../util.js");
+    util.conf.appDir = global.sandboxDir;
+
+    var gulpStream = updChangelog();
+    gulpStream.on("end", function(){
+      /* Test something here */
+      done();
+    });
+  });
+});
