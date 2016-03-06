684f4d7f7a3e2127c5d8d65491eb17fa33d3ffca
test <walker@praiseweb.com.br>
2016-03-06 09:45:25 -0300

    feat(tests): Add should bump-version patch test

--- tests/bump-version.test.js
+++ tests/bump-version.test.js
@@ -1,1 +1,15 @@
 "use strict";
+var should = require("should");
+var bumpVersion = require("../tasks/bump-version.js");
+
+describe("Task bump-version", function(){
+  it("should bump patch", function(){
+    var util = require("../util.js");
+    util.conf.appDir = global.sandboxDir;
+    var gulpStream = bumpVersion(null, "patch");
+    gulpStream.on("end", function(){
+      var version = util.packageVersion();
+      version.should.match(/\d+.\d+.\d+/);
+    });
+  });
+});
