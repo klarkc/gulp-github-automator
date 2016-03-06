3d2f42eb2cd828d576203fd3489a76fdb730f94c
test <walker@praiseweb.com.br>
2016-03-06 17:46:11 -0300

    feat(tests): Add bump-version tests

--- tests/bump-version.test.js
+++ tests/bump-version.test.js
@@ -1,15 +1,80 @@
 "use strict";
 var should = require("should");
-var bumpVersion = require("../tasks/bump-version.js");
+var fs = require("fs");
+var mockery = require("mockery");
+
+var mockOptions = {
+  warnOnUnregistered: false,
+  warnOnReplace: false,
+  useCleanCache: true
+};
 
 describe("Task bump-version", function(){
-  it("should bump patch", function(){
+  beforeEach(function(){
+    mockery.enable(mockOptions);
+  });
+
+  afterEach(function(){
+    mockery.disable();
+  });
+
+  it("should bump patch", function(done){
+    mockery.registerMock("yargs", {
+      argv: {
+        t: "patch"
+      }
+    });
+
     var util = require("../util.js");
     util.conf.appDir = global.sandboxDir;
-    var gulpStream = bumpVersion(null, "patch");
+
+    var bumpVersion = require("../tasks/bump-version.js");
+
+    var gulpStream = bumpVersion();
     gulpStream.on("end", function(){
       var version = util.packageVersion();
-      version.should.match(/\d+.\d+.\d+/);
+      version.should.equal("1.2.4");
+      done();
+    });
+  });
+
+  it("should bump minor", function(done){
+    mockery.registerMock("yargs", {
+      argv: {
+        t: "minor"
+      }
+    });
+
+    var util = require("../util.js");
+    util.conf.appDir = global.sandboxDir;
+
+    var bumpVersion = require("../tasks/bump-version.js");
+
+    var gulpStream = bumpVersion();
+    gulpStream.on("end", function(){
+      var version = util.packageVersion();
+      version.should.equal("1.3.0");
+      done();
+    });
+  });
+
+  it("should bump major", function(done){
+    mockery.registerMock("yargs", {
+      argv: {
+        t: "major"
+      }
+    });
+
+    var util = require("../util.js");
+    util.conf.appDir = global.sandboxDir;
+
+    var bumpVersion = require("../tasks/bump-version.js");
+
+    var gulpStream = bumpVersion(null, "minor");
+    gulpStream.on("end", function(){
+      var version = util.packageVersion();
+      version.should.equal("2.0.0");
+      done();
     });
   });
 });
